27f6b158b09c32b61f05bab7196c0f6d8dd9cdea
kaitlynning <kittyning88308@gmail.com>
2020-03-03 18:07:18 +0800

    list.append

---
+++ Py-practice/0303.py
@@ -0,0 +1,13 @@
+#animals list 
+animals = ['cat', 'dog', 'rabbit']
+
+#'guinea pig' is appended to the animals list
+animals.append('guinea pig')
+
+#updated animals list
+print('Updated animals list:', animals)
+
+
+'''
+list append()
+'''
