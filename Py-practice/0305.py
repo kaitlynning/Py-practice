83c964fa026d74f28d330abcb4378691266c0d0b
kaitlynning <kittyning88308@gmail.com>
2020-03-04 14:35:16 +0800

    list.append.2

---
+++ Py-practice/0305.py
@@ -0,0 +1,14 @@
+#animals list 
+animals = ['cat', 'dog', 'rabbit']
+
+#list of wild animals
+wild_animals = ['elephant', 'monkey', 'tiger']
+#appending wild_animals list to the animals list
+# animals.append(wild_animals)
+# updated animals list
+l = animals + wild_animals
+print('Updated animals list:', l)
+
+'''
+Updated animals list: ['cat', 'dog', 'rabbit', 'elephant', 'monkey', 'tiger']
+''' 
