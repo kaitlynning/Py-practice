74e19bbcda43f47b20d7a15a24e7f951caf486bf
kaitlynning <kittyning88308@gmail.com>
2020-03-05 12:09:08 +0800

    list-extend

---
+++ Py-practice/0306.py
@@ -0,0 +1,14 @@
+#language list 
+language = ['Chinese', 'English', 'Japanese', 'Cantonese']
+#another list
+language1 = ['Spanish', 'German', 'French']
+
+language.extend(language1)
+
+#extended list
+print('Language List: ', language)
+
+'''
+Language List:  ['Chinese', 'English', 'Japanese', 'Cantonese', 'Spanish', 'German', 'French']
+
+'''
