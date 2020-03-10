21566dd6eba9d7568e7a1cecd6875c83c971ebe7
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-10 13:30:27 +0800

    input practice

---
+++ Py-practice/0311.py
@@ -0,0 +1,10 @@
+favorite_fruit = input("What is your favorite fruit? ")
+
+print("Oh cool! I like " + favorite_fruit + " too, but I think my favorite fruit is watermelon.")
+
+'''
+What is your favorite fruit? apple
+
+Oh cool! I like apple too, but I think my favorite fruit is watermelon.
+
+'''
