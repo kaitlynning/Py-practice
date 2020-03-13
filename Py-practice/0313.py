ee0ddff698c2f9340ee27f39f0d61f6695024d5a
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-13 11:40:02 +0800

    Calculate dog years
    Some say that one year of a human’s life is equivalent to seven years of a dog’s life.

---
+++ Py-practice/0313.py
@@ -0,0 +1,14 @@
+def dog_years(name, age):
+	return name+", you are "+ str(age*7)+" years old in human years."
+
+
+print(dog_years("Polly", 12))
+
+print("Some say that one year of a human’s life is equivalent to seven years of a dog’s life.")
+
+
+'''
+Polly, you are 84 years old in human years.
+Some say that one year of a human’s life is equivalent to seven years of a dog’s life.
+
+'''
