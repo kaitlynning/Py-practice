a5b1e8c864ffe6df7d25a691234b2247f25c877b
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-18 21:26:59 +0800

    .append concept

---
+++ Py-practice/0319.py
@@ -0,0 +1,19 @@
+orders = ['daisies', 'periwinkle']
+
+orders.append('tulips')
+orders.append('roses')
+print(orders)
+
+'''
+It’s important to remember that 
+.append() comes after the list. 
+This is different from functions like print,
+which come before.
+
+# Create a list
+my_list = [1, 2, 3]
+
+# Append a number
+my_list.append(5)
+print(my_list) # check the result
+'''
