e8b78e204d27570b0548877e8ccf83d879b1c64f
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-20 22:52:46 +0800

    sort

---
+++ Py-practice/0320.py
@@ -0,0 +1,20 @@
+cities = ['San Fransciso', 'Paris', 'Taipei', 'Los Angeles', 'New York']
+
+cities.sort()
+print(cities)
+
+sorted_cities = cities.sort()
+print(sorted_cities)
+
+'''
+# numerical (1, 2, 3, …) or alphabetical (a, b, c, …) order
+
+# Notice that sort goes after list, cities. If we try sort(names), we will get a NameError.
+
+# sort does not return anything. So, if we try to assign cities.sort() to a variable, our new variable would be None:
+
+['Los Angeles', 'New York', 'Paris', 'San Fransciso', 'Taipei']
+
+None
+
+'''
