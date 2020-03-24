d6f0760dc46730abdd36f024159426c74f96e002
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-24 23:13:12 +0800

    Range in looops

---
+++ Py-practice/0324.py
@@ -0,0 +1,15 @@
+promise = "I will always love you"
+
+for i in range(5):
+  print(promise)
+
+'''
+# USING RANGE IN LOOPS
+
+I will always love you
+I will always love you
+I will always love you
+I will always love you
+I will always love you
+
+'''
