2d7e1aa787f2a4177bbd1d22cfde0be2b1f17ac5
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-27 12:02:51 +0800

    List comprehensions: temp convert

---
+++ Py-practice/0329.py
@@ -0,0 +1,13 @@
+celsius = [0, 10, 15, 32, -5, 27, 3]
+
+fahrenheit = [temp*(9/5) + 32 for temp in celsius]
+
+print(fahrenheit)
+
+'''
+# list comprehensions: temp convert
+# celsius
+# fahrenheit
+
+[32.0, 50.0, 59.0, 89.6, 23.0, 80.6, 37.4]
+'''
