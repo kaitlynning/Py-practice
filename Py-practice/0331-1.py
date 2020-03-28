6aed70cb90095abc412574cd89e9ec802a54f6c9
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-28 22:19:15 +0800

    While loops check element if TRUE

--- Py-practice/0331-1.py
+++ Py-practice/0331-1.py
@@ -6,7 +6,6 @@ def delete_starting_evens(lst):
     lst = lst[1:]
   return lst
 
-#Uncomment the lines below when your function is done
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
 print(delete_starting_evens([4, 8, 10]))
 
