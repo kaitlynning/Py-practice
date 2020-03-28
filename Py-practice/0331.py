2ad6628868b24f131b87bc9245d038f121844daf
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-03-28 20:25:43 +0800

    Greetings: Hello Kitty

---
+++ Py-practice/0331.py
@@ -0,0 +1,15 @@
+def add_greetings(names):
+  new_list = []
+  for name in names:
+    new_list.append("Hello, " + name)
+  return new_list
+
+print(add_greetings(["Kitty", "Kitty", "Kitty"]))
+
+
+
+'''
+# add greetings
+['Hello, Kitty', 'Hello, Kitty', 'Hello, Kitty']
+
+'''
