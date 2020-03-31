cea47f4775763c6b2117ad878af65becf594aacc
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-01 00:03:06 +0800

    Cut a Slice of String

---
+++ Py-practice/0403-1.py
@@ -0,0 +1,16 @@
+first_name = "Kaitlyn"
+last_name = "Kaitlyn"
+
+new_account = last_name[:4]
+
+temp_password = last_name[4:8]
+
+print(new_account)
+print(temp_password)
+
+
+'''
+# Cut a Slice of String
+Kait
+lyn
+'''
