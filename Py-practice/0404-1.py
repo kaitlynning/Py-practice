05f6a35a463282c83bcfd57f81688cba89b83280
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-02 13:50:09 +0800

    Negative indices

---
+++ Py-practice/0404-1.py
@@ -0,0 +1,18 @@
+motto = "You will never be completely at home again. Because part of your heart will always be elsewhere. That is the price you pay for the richness of loving and knowing people in more than one place"
+
+second_to_last = motto[-2]
+
+highlight_word = motto[-48:]
+
+print(motto)
+print(second_to_last)
+print(highlight_word)
+
+
+'''
+# Negative indices
+You will never be completely at home again. Because part of your heart will always be elsewhere. That is the price you pay for the richness of loving and knowing people in more than one place
+c
+loving and knowing people in more than one place
+
+'''
