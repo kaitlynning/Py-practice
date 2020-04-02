7fe23b2937fdc83b4f4c4409ec328bb6a857ee53
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-02 15:52:21 +0800

    Escape characters

---
+++ Py-practice/0404-2.py
@@ -0,0 +1,15 @@
+# We’ll have accidentally ended the string before we wanted to by including the " character.
+# By adding a backslash in front of the special character we want to escape, \", we can include it in a string.
+
+#theycallme"crazy"91
+password = "theycallme\"Kitty\"93"
+#His password was causing some errors in the system because of the " marks. Rewrite his password using escape characters and save it to the variable password.
+print(password)
+
+
+
+'''
+
+theycallme"Kitty"93
+
+'''
