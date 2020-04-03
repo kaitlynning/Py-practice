7bf3b773c6632f432d6c6835330aa12c932130c1
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-03 23:51:46 +0800

    Splitting strings

---
+++ Py-practice/0405.py
@@ -0,0 +1,16 @@
+line_one = "Life is like a box of chocolate"
+
+line_one_words = line_one.split()
+
+print(line_one_words)
+
+
+
+'''
+#splitting strings
+#.upper(), .lower(), and .title() all are performed on an existing string and produce a string in return.
+#.split() is performed on a string, takes one argument, and returns a list of substrings found between the given argument
+
+['Life', 'is', 'like', 'a', 'box', 'of', 'chocolate']
+
+'''
