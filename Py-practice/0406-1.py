b3cfab69d3c734da2136fd0fee763de71378895e
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-04 23:12:57 +0800

    string method: format

---
+++ Py-practice/0406-1.py
@@ -0,0 +1,15 @@
+
+def poem_title_card(poet, title):
+  # \"{}\"
+  poem_desc = "The poem \"{}\" is written by {}.".format(title, poet)
+  return poem_desc
+
+print("The poem \"{}\" is written by {}.".format("Walt Whitman", "I Hear America Singing"))
+
+'''
+#strings methods
+#.format()
+# \"{}\"
+
+The poem "Walt Whitman" is written by I Hear America Singing.
+'''
