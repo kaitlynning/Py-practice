a77c25d036cd735c10cdd10dc79a12b823f42249
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-04 23:02:06 +0800

    string method: format

---
+++ Py-practice/0406.py
@@ -0,0 +1,21 @@
+def favorite_song_statement(song, artist):
+  # {}import variables
+  return "My favorite song is {} by {}.".format(song, artist)
+
+print("My favorite song is {} by {}.".format('Fresh eyes', 'Andy Grammer'))
+
+
+
+'''
+#strings methods
+#.format()
+
+My favorite song is Fresh eyes by Andy Grammer.
+
+
+
+# ' '.join & '\n'.join v.s. .split()
+# .find() 
+# .replace(a,b) = replace a with b
+# .strip() = removes all whitespace characters from the beginning and end.
+'''
