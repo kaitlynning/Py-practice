73540b225599b7957b4892e108cf20b3168912d6
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-23 22:01:59 +0800

    Runtime: 24 ms, faster than 99.49%

---
+++ 21. Merge Two Sorted Lists.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l2.next,l1)
+            return l2
+
+
+'''
+Leetcode-Easy
+21. Merge Two Sorted Lists
+Runtime: 24 ms, faster than 99.49%
+'''
