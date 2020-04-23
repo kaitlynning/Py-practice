f890821cc8f70128ace40f4b81c23e93fac5128a
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-23 13:22:25 +0800

    add_exclamation()

---
+++ Py-practice/0422.py
@@ -0,0 +1,13 @@
+def add_exclamation(word):
+  while(len(word) < 20):
+    word += "!"
+  return word
+
+print(add_exclamation("Learn to code"))
+
+print(add_exclamation("And soon you will become a woman in tech."))
+
+'''
+Learn to code!!!!!!!
+And soon you will become a woman in tech.
+'''
