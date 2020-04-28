b87e4c7771bb82d13a46afc20d1f5e14b8934eac
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-04-28 23:58:53 +0800

    Runtime: 32 ms, faster than 78.19%

---
+++ 38. Count and Say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for i in range(n-1):
+            count = 1
+            temp = []
+            for index in range(1, len(s)):
+                if s[index] == s[index-1]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
+        return s 
+
+'''
+Leetcode-Easy
+38. Count and Say
+Runtime: 32 ms, faster than 78.19%
+'''
