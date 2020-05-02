ac6a1c065bc396d1bcc42bad6405602d84f86a05
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-02 23:02:00 +0800

    Runtime: 68 ms, faster than 5.36%

---
+++ 67. Add Binary.py
@@ -0,0 +1,24 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry %2)
+            carry //= 2
+
+        return result[::-1]
+        
+'''
+Leetcode-Easy
+67. Add Binary
+Runtime: 68 ms, faster than 5.36%
+'''
