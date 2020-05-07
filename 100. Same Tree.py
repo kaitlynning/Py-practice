9c01f5f248eb0a5682e7fda014feda5e6800895b
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-07 21:35:43 +0800

    Runtime: 28 ms, faster than 69.08%

---
+++ 100. Same Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.right, q.right) and self.isSameTree(q.left, p.left)
+        return q == p
+            
+
+'''
+Leetcode-Easy
+100. Same Tree
+Runtime: 28 ms, faster than 69.08% 
+'''
