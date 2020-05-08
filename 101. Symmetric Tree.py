8dd0b4a7d08f8837affdc35b4e91b7ce35d2d48f
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-08 17:56:59 +0800

    Runtime: 28 ms, faster than 90.02%

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        else:
+            return self.isSymmetricRecu(root.left, root.right)
+        
+    def isSymmetricRecu(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None or left.val != right.val:
+            return False
+        return self.isSymmetricRecu(left.left, right.right) and self.isSymmetricRecu(left.right, right.left)
+
+'''
+Leetcode-Easy
+101. Symmetric Tree
+Runtime: 28 ms, faster than 90.02%
+'''
