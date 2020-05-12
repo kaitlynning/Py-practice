8744d41d2c258236b7755bfa246e76729ed20f95
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-12 16:06:49 +0800

    File

---
+++ Py-practice/0512.py
@@ -0,0 +1,11 @@
+with open('file.txt') as file_object:
+  print(file_object.read())
+
+#Open up file objects using open() and with.
+#Read a file’s full contents using Python’s .read() method.
+#Read a file line-by-line using .readline() and .readlines()
+'''
+Python:Files
+Thank you for learning about files in Python with us!
+
+'''
