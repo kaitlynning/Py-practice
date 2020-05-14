c34cb9d4f5aff7c6abcda90cbcd03f4d41308b42
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-15 00:01:54 +0800

    Runtime: 36 ms, faster than 95.66%

--- 111. Minimum Depth of Binary Tree.py
+++ 111. Minimum Depth of Binary Tree.py
@@ -8,7 +8,7 @@ class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if root is None:
             return 0
-        
+        #input 不存在child or root 無法count則 return 0
         if root.left and root.right:
             return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
         else:
