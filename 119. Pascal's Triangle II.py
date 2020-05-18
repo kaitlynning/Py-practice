99dab3516a33d1532431dddb89b51b9d08b9f6bd
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-18 12:58:09 +0800

    Runtime: 20 ms, faster than 98.39%

--- 119. Pascal's Triangle II.py
+++ 119. Pascal's Triangle II.py
@@ -1,9 +1,9 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        result = [1]*rowIndex + [1]
+        result =[1] + [0]*rowIndex 
         
-        for i in range(2, rowIndex+1):
-            for j in range(i-1,0,-1):
+        for i in range(rowIndex):
+            for j in range(i+1,0,-1):
                 result[j] += result[j-1]
             
         return result
@@ -11,7 +11,7 @@ class Solution:
 '''
 Leetcode-Easy
 119. Pascal's Triangle II
-Runtime: 52 ms, faster than 5.15%
+Runtime: 20 ms, faster than 98.39%
 
 
 '''
