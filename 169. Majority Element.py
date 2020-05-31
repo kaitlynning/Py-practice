40ae80d8c020f400ffc8758089f294963d474687
kaitlynning <56193342+kaitlynning@users.noreply.github.com>
2020-05-31 23:25:43 +0800

    Runtime: 172 ms, faster than 74.47%

---
+++ 169. Majority Element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        index, count = 0, 1
+        for i in range (1, len(nums)):
+            if nums[index] == nums[i]:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    index = i
+                    count = 1
+        return nums[index]
+        
+        
+'''
+Leetcode-Easy
+169. Majority Element
+Runtime: 172 ms, faster than 74.47%
+
+
+
+'''
